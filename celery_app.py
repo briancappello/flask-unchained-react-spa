a00230679d2af409ecc1a97df1e591cdd6b7d2cf
Brian Cappello <briancappello@gmail.com>
2020-01-07 22:45:55 -0500

    app factory must be instantiated now

--- celery_app.py
+++ celery_app.py
@@ -1,9 +1,10 @@
 import os
 
-# we import this here so celery can access it for its startup
-from flask_unchained.bundles.celery import celery
 from flask_unchained import AppFactory, PROD
 
+# import the celery extension so that celery can access it for its startup
+from flask_unchained.bundles.celery import celery
 
-app = AppFactory.create_app(os.getenv('FLASK_ENV', PROD))
+
+app = AppFactory().create_app(os.getenv('FLASK_ENV', PROD))
 app.app_context().push()
